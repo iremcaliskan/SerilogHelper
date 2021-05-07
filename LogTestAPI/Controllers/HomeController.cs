1f914d0bc31454960a62105d8d2b7aea0ff316bd
iremcaliskan <iremcaliskan@eskisehir.edu.tr>
2021-05-07 12:41:12 +0300

    Sample object passed as information logging in HomeController, FileLogManager

--- LogTestAPI/Controllers/HomeController.cs
+++ LogTestAPI/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace LogTestAPI.Controllers
         { // Ex. Logging is done when Index runs - Örneğin, Index çalıştığı an loglama yapılacak
             var message = new { FirstName = "İrem", LastName = "Çalışkan", Message = "Bu bir test mesajıdır." };
             LoggerFactory.DatabaseLogManager().Information("{@message}", message);
-            LoggerFactory.FileLogManager().Information("test");
+            LoggerFactory.FileLogManager().Information("{@message}", message);
             return View();
         }
     }
